00ac8e4d7ab370a9bd5185ff270b5a0dbe24531c
jommer <ramosjommer24@gmail.com>
2021-05-29 07:15:08 +0800

    Initial commit

---
+++ Lab1/App.js
@@ -0,0 +1,19 @@
+// VicedoGabrielV.
+//BSCS 3-4
+//
+import React from 'react';
+import { View, Text} from 'react-native';
+
+
+export default function App(){
+return(
+   <View style={{
+      flex:1,
+      alignItems:'center',
+      justifyContent:'center',
+      background:'red'
+      }}>
+      <Text>Hello World</Text>
+      </View>
+);
+}
